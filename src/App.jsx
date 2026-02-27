c58c0ae569ce0a733fe72528ac828cb8d091530b
Kamivision <kamivision@gmail.com>
2026-02-27 15:53:52 -0600

    updated App and Favorites Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
 import { Outlet } from "react-router-dom";
 import Banner from "./components/Banner";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
 
 
 function App() {
-  const { id } = useParams();
+  
   const [character, setCharacter] = useState(null);
   const [favorites, setFavorites] = useState([]);
 
-  useEffect(() => {
-  const fetchCharacter = async () => {
+  // const getCharacterById = async (id) => {
+    
+  //   let requestURL = `https://rickandmortyapi.com/api/character/${id}`;
+  //   console.log(requestURL);
+    
+  //   try {
+  //     let response = await axios.get(requestURL);
+  //     return response.data;
+    
+  //   } catch (err) {
+  //     console.error(err);
+  //     return null;
+  //   }
+  // };
+
+  const fetchCharacter = async (id) => {
     let requestURL = `https://rickandmortyapi.com/api/character/${id}`;
 
     try {
       let response = await axios.get(requestURL);
     //   let data =response.data;
-      setCharacter(response.data);
+      // setCharacter(response.data);
+        let data = response.data; // Use .results for character array
     } catch (err) {
       console.error(err);
     }
   };
+ 
 
-  fetchCharacter();
-  }, [id]);
+  const addData = (data) => {
+    setCharacter(data)(
+      [...character, data]
+    )}
+
 
   const createFav = (char) => {
     setFavorites(current => {
@@ -50,6 +69,8 @@ function App() {
       <Banner />
       <Outlet context={{
         setCharacter,
+        fetchCharacter,
+        addData,
         character,
         favorites,
         createFav,
--- src/pages/FavoriteCharactersPage.jsx
+++ src/pages/FavoriteCharactersPage.jsx
@@ -1,16 +1,18 @@
 import { useOutletContext } from 'react-router-dom';
 import CharacterCard from '../components/CharacterCard';
+import Row from 'react-bootstrap/Row';
 
 const FavoriteCharactersPage = () => {
   const { favorites = [], removeFavorite, createFav } = useOutletContext();
 
   return (
       <>
+          <img src='/Rick_and_Morty.svg' className='block-30 justify-self-center' />
           <h1 className="text-3xl font-bold mb-4">Favorite Characters</h1>
           {favorites.length === 0 ? (
             <p>You haven't added any favorites yet.</p>
           ) : (
-            <div className="columns-4 gap-2">
+            <Row xs="auto" md="auto" className="g-2">
               {favorites.map(char => (
                 <CharacterCard
                   key={char.id}
@@ -24,7 +26,7 @@ const FavoriteCharactersPage = () => {
                   favorites={favorites}
                 />
               ))}
-            </div>
+            </Row>
           )}
       </>
   )
